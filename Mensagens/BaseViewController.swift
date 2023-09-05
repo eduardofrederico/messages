417731eb584c515e403da393462d535c7c4aacb7
Eduardo Frederico <eduardo.frederico@icloud.com>
2023-09-04 23:35:14 -0300

    Initial Commit

---
+++ Mensagens/BaseViewController.swift
@@ -0,0 +1,27 @@
+//
+//  BaseViewController.swift
+//  Mensagens
+//
+//  Created by Eduardo Frederico on 28/08/23.
+//  Copyright © 2023 Eric Brito. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    @IBOutlet weak var lbMessage: UILabel!
+    var message: Message!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func changeColor(_ sender: UIButton) {
+        
+    }
+
+
+}
